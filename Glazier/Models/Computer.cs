5e02bc7e502445f60afec28c789b2e2fb9e45813
Ian Steiger <iksteiger@gmail.com>
2022-11-13 20:28:50 -0500

    things

---
+++ Glazier/Models/Computer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glazier.Models;
+
+public class Computer
+{
+    public string DisplayName
+    {
+        get; set;
+    }
+    public string HostName
+    {
+        get; set;
+    }
+    public string UserName
+    {
+        get; set;
+    }
+    public string Password
+    {
+        get; set;
+    }
+    public string MacAddress
+    {
+        get; set;
+    }
+}
