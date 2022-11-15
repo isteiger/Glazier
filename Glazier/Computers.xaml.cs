2e8b580d16a5239431b276deb0e383de5ba6d55c
Ian Steiger <iksteiger@gmail.com>
2022-11-15 10:45:20 -0500

    jailfkds

--- Glazier/Computers.xaml.cs
+++ Glazier/Computers.xaml.cs
@@ -1,20 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Storage.Pickers;
-using Windows.Storage;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Glazier.Models;
 using System.Collections.ObjectModel;
@@ -63,7 +50,7 @@ public sealed partial class Computers : Page {
 
         if (file != null) {
             // Application now has read/write access to the picked file
-            ComputersJSONFile = file;
+            ComputersJSONFile = file.Path;
             String json = File.ReadAllText(file.Path);
             JObject o = JObject.Parse(json);
             foreach (JToken puter in o["computers"]){
